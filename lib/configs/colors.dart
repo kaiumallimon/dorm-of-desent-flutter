5eca714646b33a980794263a0671a61366095efb
Kaium Al Limon <limon.3cs@gmail.com>
2025-11-28 04:00:57 +0600

    Add AppColors class for centralized color management

---
+++ lib/configs/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = CupertinoColors.activeBlue;
+  static const secondary = CupertinoColors.activeOrange;
+  static const lightBackground = CupertinoColors.systemGrey6;
+  static const darkBackground = CupertinoColors.darkBackgroundGray;
+  static const lightForeground = CupertinoColors.label;
+  static const darkForeground = CupertinoColors.white;
+  static const onPrimary = CupertinoColors.white;
+  static const onSecondary = CupertinoColors.black;
+}
